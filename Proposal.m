69ccad2708c79025568fe323dd08617d18efdc0c
thanooshakarri <122545991+thanooshakarri@users.noreply.github.com>
2023-02-26 21:39:39 -0700

    Create Proposal.md

---
+++ Proposal.m
@@ -0,0 +1,32 @@
+# MENTAL HEALTH IN TECH
+# Names
+Thanoosha Naidu Karri, Sai Madhuri Kandula, Priyarsha Vallabhaneni.
+
+# Brief Description
+## Background
+
+Mental health became equally important as the physical health of an individual. Many organizations from startups to multinational companies need to take proper measures to ensure the mental well being of their employees. A survey is conducted to collect data from employees in different fields that measures attitude towards mental health and frequency of mental health disorders. Based on the data collected we aim to build a model that helps us to predict if a person is in need for treatment.
+
+## Why It is Important?
+Not only do the organisation need to ensure its own growth but also its employee's well-being as they are the building blocks for the company's success. Most of the employees may feel embarrassed regarding their mental health issues and try to be secretive about it in order for them to not be treated unfairly by either their higher ups or their under subs. So this project is important as this helps in detecting if a particular individual requires any immediate guidance for their issues.
+
+## Knowledge Gap(What we know/What we don't Know):
+-We don't know the details about the workload and the work-life balance of the employees which might be factors affecting the mental health.
+-We know about the work environment of the employees, mindset of the supervisor and the co-workers.                                              
+-We don't entirely know the accuracy and credibility of the data as the respondents may not be comfortable to share their concerns.                               
+-Also, a part of the data collected is not entirely complete like the comments attribute in the dataset, which has more than 50% of the data missing.
+
+## Brief description of data required and How would you get such data
+
+We need data from different employees working in different departments, in different organizations. This data must include demographic information like age, gender, employment details. Apart from this we also need the details of type of workspace the employee is working in which requires data of the supervisors and also the co-workers.
+We collected the dataset from openml.
+https://www.openml.org/search?type=data&status=active&id=43674&sort=runs
+
+## A list of questions and corresponding analysis tasks you plan to do
+
+1. Trying different approaches to find the best model having high accuracy that helps us to predict if a person needs treatment for his/her mental issues.
+2. What are that factors that impact a person's mental health the most?
+3. Type of work environment which is ideal and worse for employees mental well-being.
+
+# References
+* https://www.openml.org/search?type=data&status=active&id=43674
